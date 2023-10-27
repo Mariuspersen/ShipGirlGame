030b550fa74f0781ffb1de484062bfd2d9b26f54
Marius Alexandersen <marius_kule@hotmail.com>
2023-10-27 15:23:35 +0200

    more wfc changes

--- scripts/wavefunctioncollapse.js
+++ scripts/wavefunctioncollapse.js
@@ -1,12 +1,21 @@
 function wfc(tiles,tilecount_x,tilecount_y,tileset) {
     initial_tile = random_int(tiles.length)
     initial_tileimage = random_int(tileset.length)
-    tiles[initial_tile].image = tileset[initial_tileimage]
+    tiles[initial_tile].image = tileset[0]
 
     const neighbors = find_neighbors(tiles,initial_tile,tilecount_x,tilecount_y)
-    neighbors.forEach(tile => {
-        tile.image = tileset[random_int(tileset.length)]
+    neighbors.forEach(tile_index => {
+        determine_image(tiles,tile_index,tilecount_x,tilecount_y,tileset)
     });
+    
+
+    
+}
+
+function determine_image(tiles,current_tile,tilecount_x,tilecount_y,tileset) {
+    const neighbors = find_neighbors(tiles,current_tile,tilecount_x,tilecount_y)
+    tiles[current_tile].image = tileset[0]
+    
 }
 
 function random_int(max) {
@@ -19,24 +28,31 @@ function find_neighbors(tiles, tile_index ,tilecount_x,tilecount_y) {
     const y = Math.floor(tile_index / tilecount_x)
 
     if (x > 0) {
-        neighbors.push(tiles[y * tilecount_x + (x - 1)]);
+        neighbors.push(y * tilecount_x + (x - 1));
     }
 
     // Right neighbor
     if (x < tilecount_x - 1) {
-        neighbors.push(tiles[y * tilecount_x + (x + 1)]);
+        neighbors.push(y * tilecount_x + (x + 1));
     }
 
     // Top neighbor
     if (y > 0) {
-        neighbors.push(tiles[(y - 1) * tilecount_x + x]);
+        neighbors.push((y - 1) * tilecount_x + x);
     }
 
     // Bottom neighbor
     if (y < tilecount_y - 1) {
-        neighbors.push(tiles[(y + 1) * tilecount_x + x]);
+        neighbors.push((y + 1) * tilecount_x + x);
     }
 
     return neighbors;
 
+}
+
+const direction = {
+    top: 0,
+    bottom: 1,
+    right: 2,
+    left: 3,
 }
